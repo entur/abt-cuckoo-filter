63f2ed47bbb793b7dd7707a4fde2e9de7a51b143
Markus Hauge <markus.hauge@entur.org>
2025-12-16 14:45:33 +0100

    Set up project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+}
+
+group = "no.entur"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
