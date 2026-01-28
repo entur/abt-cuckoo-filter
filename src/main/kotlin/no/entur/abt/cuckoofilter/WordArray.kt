bfed74091804533d7a0986e92031196b23c9300b
Ole Reidar Holm <holm.ole@outlook.com>
2026-01-28 09:55:15 +0100

    Update comment in WordArray.kt to reflect Math.ceilDiv() availability in Java 18

--- src/main/kotlin/no/entur/abt/cuckoofilter/WordArray.kt
+++ src/main/kotlin/no/entur/abt/cuckoofilter/WordArray.kt
@@ -20,7 +20,7 @@ class WordArray(
      * The underlying byte array is sized to hold exactly [size] words of [wordBits] bits each,
      * rounded up to the nearest byte boundary.
      *
-     * Note: Math.ceilDiv() was introduced in Java 9 and is not available in Java 8.
+     * Note: Math.ceilDiv() was introduced in Java 18 and is not available in Java 8.
      * Therefore, we use the formula (a + b - 1) / b to compute ceiling division:
      * (size * wordBits + 7) / 8 is equivalent to Math.ceilDiv(size * wordBits, 8).
      * This ensures we allocate enough bytes to store all bits without truncation.
